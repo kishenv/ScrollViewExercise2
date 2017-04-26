bc86453221fb5a68778ce1770d88c8d77091b2d5
Kishen Vinod <kishenv@gmail.com>
2017-04-26 11:25:35 -0500

    3rd commit fixed constraints

--- PageTheScroll2/ViewController.swift
+++ PageTheScroll2/ViewController.swift
@@ -21,9 +21,14 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         
+      
+    }
+    override func viewDidAppear(_ animated: Bool) {
         var contentWidth: CGFloat = 0.0                 //sets width of UI Scroll
         
-        print("Scroll View Width: \(scrollView.frame.width)")
+        print("Scroll View Width: \(scrollView.frame.size.width)")
+        
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2
         {
@@ -31,16 +36,19 @@ class ViewController: UIViewController
             let imageView = UIImageView(image:image)    //creates a constant imageView of type UIImageVIew
             images.append(imageView)
             
+            
+            
+            
             var newX: CGFloat = 0.0                     // x axis point for current image
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
-                                                        //sets x values of all images during each iteration..
+            newX = scrollWidth/2 + scrollWidth * CGFloat(x)
+            //sets x values of all images during each iteration..
             
             contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX-75, y: (view.frame.size.height/2)-75, width: 150, height: 150)
-                                                        //sets x,y,width and height of UIImage
+            imageView.frame = CGRect(x: newX-75, y: (scrollView.frame.size.height/2)-75, width: 150, height: 150)
+            //sets x,y,width and height of UIImage
             
             
             print ("run instance")
@@ -49,6 +57,7 @@ class ViewController: UIViewController
         scrollView.backgroundColor = UIColor.purple
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
         print ("Count:\(images.count)")
+        
     }
 
 
